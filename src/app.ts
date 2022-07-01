088f24e0d9077cbe05c34408c98a088b50259582
EkeMinusYou <euonymuslke+develop@gmail.com>
2022-07-01 21:10:17 +0900

    simplify

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,33 @@
-import { join } from 'path';
-import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
-import { FastifyPluginAsync } from 'fastify';
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
 
-export type AppOptions = {
-  // Place your custom options for app below here.
-} & Partial<AutoloadPluginOptions>;
+const server: FastifyInstance = Fastify({})
 
-const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
-): Promise<void> => {
-  // Place here your custom code!
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          pong: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
+}
 
-  // Do not touch the following lines
+server.get('/ping', opts, async (request, reply) => {
+  return { pong: 'it worked!' }
+})
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts
-  })
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 })
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    options: opts
-  })
-
-};
-
-export default app;
-export { app }
+start()
